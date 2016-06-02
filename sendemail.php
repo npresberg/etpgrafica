8a976aaaf09b9bf83b5066ebd1af688300a6f9bb
npresberg <npresberg@gmail.com>
2016-06-02 12:26:39 -0300

    formulario

--- sendemail.php
+++ sendemail.php
@@ -19,3 +19,39 @@
 
     echo json_encode($status);
     die;
+
+
+<?php
+if(isset($_POST['email'])) {
+
+// Debes editar las próximas dos líneas de código de acuerdo con tus preferencias
+$email_to = "info@etpgrafica.com.ar";
+$email_subject = "Contacto desde el sitio web";
+
+// Aquí se deberían validar los datos ingresados por el usuario
+if(!isset($_POST['name']) ||
+!isset($_POST['subject']) ||
+!isset($_POST['email']) ||
+!isset($_POST['message'])) {
+
+echo "<b>Ocurrió un error y el formulario no ha sido enviado. </b><br />";
+echo "Por favor, vuelva atrás y verifique la información ingresada<br />";
+die();
+}
+
+$email_message = "Detalles del formulario de contacto:\n\n";
+$email_message .= "Nombre: " . $_POST['name'] . "\n";
+$email_message .= "Empresa: " . $_POST['subject'] . "\n";
+$email_message .= "E-mail: " . $_POST['email'] . "\n";
+$email_message .= "Mensaje: " . $_POST['message'] . "\n\n";
+
+
+// Ahora se envía el e-mail usando la función mail() de PHP
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $subject, $body, 'From: <'.$email_from.'>');
+
+echo "¡El formulario se ha enviado con éxito!";
+}
+?>
