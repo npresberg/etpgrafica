c382c2a7eb758cd2a1916f5eb518f261976a366e
npresberg <npresberg@gmail.com>
2016-06-06 22:26:04 -0300

    mail

--- sendemail.php
+++ sendemail.php
@@ -1,57 +1,31 @@
 <?php
-	header('Content-type: application/json');
-	$status = array(
-		'type'=>'success',
-		'message'=>'Email sent!'
-	);
+	$name = $_POST['name'];
+	$company = $_POST['company'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
 
-    $name = @trim(stripslashes($_POST['name'])); 
-    $email = @trim(stripslashes($_POST['email'])); 
-    $subject = @trim(stripslashes($_POST['subject'])); 
-    $message = @trim(stripslashes($_POST['message'])); 
+	// Aquí se deberían validar los datos ingresados por el usuario
+	if(!isset($name) || !isset($email) ||!isset($message)) {
+		echo '{"type":"error","message":"El formulario tiene un error, por favor corrijalo y vuela a enviarlo"}';
+		die();
+	}
 
-    $email_from = $email;
-    $email_to = 'email@gmail.com';
+	// Debes editar las próximas dos líneas de código de acuerdo con tus preferencias
+	$email_to = "npresberg@gmail.com";//"info@etpgrafica.com.ar";
+	$subject = "Contacto desde el sitio web";
 
-    $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
-
-    $success = @mail($email_to, $subject, $body, 'From: <'.$email_from.'>');
-
-    echo json_encode($status);
-    die;
-
-
-<?php
-if(isset($_POST['email'])) {
-
-// Debes editar las próximas dos líneas de código de acuerdo con tus preferencias
-$email_to = "info@etpgrafica.com.ar";
-$email_subject = "Contacto desde el sitio web";
-
-// Aquí se deberían validar los datos ingresados por el usuario
-if(!isset($_POST['name']) ||
-!isset($_POST['subject']) ||
-!isset($_POST['email']) ||
-!isset($_POST['message'])) {
-
-echo "<b>Ocurrió un error y el formulario no ha sido enviado. </b><br />";
-echo "Por favor, vuelva atrás y verifique la información ingresada<br />";
-die();
-}
-
-$email_message = "Detalles del formulario de contacto:\n\n";
-$email_message .= "Nombre: " . $_POST['name'] . "\n";
-$email_message .= "Empresa: " . $_POST['subject'] . "\n";
-$email_message .= "E-mail: " . $_POST['email'] . "\n";
-$email_message .= "Mensaje: " . $_POST['message'] . "\n\n";
+	$email_message = "Detalles del formulario de contacto:\n\n";
+	$email_message .= "Nombre: " . $name . "\n";
+	$email_message .= "Empresa: " . $company . "\n";
+	$email_message .= "E-mail: " . $email . "\n";
+	$email_message .= "Mensaje: " . $message . "\n\n";
 
 
-// Ahora se envía el e-mail usando la función mail() de PHP
-$headers = 'From: '.$email_from."\r\n".
-'Reply-To: '.$email_from."\r\n" .
-'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $subject, $body, 'From: <'.$email_from.'>');
+	// Ahora se envía el e-mail usando la función mail() de PHP
+	$headers = 'From: '.$email_from."\r\n".
+		'Reply-To: '.$email_from."\r\n" .
+		'X-Mailer: PHP/' . phpversion();
+	$sucess = @mail($email_to, $subject, $body, 'From: <'.$email_from.'>');
 
-echo "¡El formulario se ha enviado con éxito!";
-}
+  echo '{"type":"success","message":"Mail enviado!"}';
 ?>
